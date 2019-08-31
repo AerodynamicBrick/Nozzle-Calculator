01dfce297e13bcc6fae85b91c7106849e0c2820b
AerodynamicBrick <aerothebrick@gmail.com>
2019-08-31 07:22:57 -0400

    Added ipt generation. excel integration. etc

--- nozzle sim v2.py
+++ nozzle sim v2.py
@@ -1,9 +1,12 @@
-#hT1ps://www.grc.nasa.gov/WWW/K-12/airplane/rkT1hsum.html
-#hT1ps://www.grc.nasa.gov/WWW/K-12/airplane/isentrop.html
-#hT1ps://www.grc.nasa.gov/www/k-12/airplane/mflchk.html
+#huzel and huang
+#Rocket Propulsion Elements
+#www.grc.nasa.gov/
+#http://www.aspirespace.org.uk
+#Rao
 
 import math
 import sys
+import openpyxl
 
 
 p1=6894757.29318 #1 is chamber pressure
@@ -36,6 +39,8 @@ elif len(sys.argv) == 1:
 """
 def Fahrenheit_to_Kelvin(F):
     return 273.5 + ((F - 32.0) * (5.0/9.0))
+def Meters_to_Inches(inch):
+    return 39.370*inch
 
 pi=3.14159265
 R=8.314462 #gas constant
@@ -54,9 +59,7 @@ V1=Rspecific*T1/p1 #page 55
 Vt=V1*((k+1)/2)**(1/(k-1)) #page 57
 while True:
     print("""Select input parameter by number
-    1. Mass Flow
-    2. Throat radius
-    3. Thrust""")
+    1. Mass Flow""")
     mode=input()
     if mode=="1":
         mdot=float(input("Input Desired Mass Flow in kg/s"))
@@ -68,14 +71,16 @@ while True:
         A2overAt=((1+M2**2*(k-1)/2)**((k+1)/(k-1)/2))*(((k+1)/2)**(-1*((k+1)/(k-1)/2)))/M2
         A2=A2overAt*At
         break
+    else:
+        print("Input not recognised. Please try again.")
+    """
     elif mode=="2":
         r=float(input("Input Desired throat radius in meters"))
         At=pi*(r**2) #area of a circle from radius
         A2=At/((((k+1)/2)**(1/(k-1)))*((p2/p1)**(1/k))*math.sqrt(((k+1)/(k-1))*(1-((p2/p1)**((k-1)/k)))))
         mdot=At*p1*k*((2/(k+1))**((k+1)/(k-1)))/math.sqrt(k*Rspecific*T1)
-        break
-    else:
-        print("Input not recognised. Please try again.")
+        break"""
+
 
 
 F = mdot * v2 + (p2 - p3) * A2
@@ -88,21 +93,58 @@ print("Mach at exit: "+str(round(M2, 4))+"\n")
 #print("p1: "+str(round(p1, 4))) #given
 #print("T1: "+str(round(T1, 4))+"\n") #given
 
-print("vt: "+str(round(vt, 4))) #HH p10
-print("pt: "+str(round(pt, 4))) #HH p9 close nuf
-print("At: "+str(round(At, 4))) #HH p9
-print("Tt: "+str(round(Tt, 4))+"\n") #HH pg 9
+print("vt: "+str(round(vt, 6))) #HH p10
+print("pt: "+str(round(pt, 6))) #HH p9 close nuf
+print("At: "+str(round(At, 8))) #HH p9
+print("Tt: "+str(round(Tt, 6))+"\n") #HH pg 9
 
-print("v2: "+str(round(v2, 4))) #HH pg 10 close nuf
+print("v2: "+str(round(v2, 6))) #HH pg 10 close nuf
 #print("p2: "+str(round(p2, 4))) #given
-print("A2: "+str(round(A2, 4))) #HH p10
-print("T2: "+str(round(T2, 4))+"\n") #HH p9
+print("A2: "+str(round(A2, 8))) #HH p10
+print("T2: "+str(round(T2, 6))+"\n") #HH p9
 
 #print("p3: "+str(round(p3, 4))) #given
 
-print("p2/p1: "+str(round(p2/p1, 4))) #HH
-print("A2/At: "+str(round(A2/At, 4))) ##HH pg8
-print("Tt/T2: "+str(round(T2/T1, 4))) #investigate further
+print("p2/p1: "+str(round(p2/p1, 6))) #HH
+print("A2/At: "+str(round(A2/At, 8))) ##HH pg8
+print("Tt/T2: "+str(round(T2/T1, 6))) #investigate further
 
-print("Specific Gas constant: " + str(Rspecific))
+print("Specific Gas constant: " + str(round(Rspecific,6)))
 
+genfileq=input("Generate a file? (yes/no)").lower()
+if(genfileq=="yes"):
+    print("look up desired Rao exit and throat angles. https://i.imgur.com/gq6j8UO.png")
+
+    wb = openpyxl.load_workbook('partgen.xlsx') #name of the included file
+    ws = wb.active #default sheet?
+
+    ws['A1']="Parameter Name"
+    ws['B1']="Value"
+    ws['C1']="Units"
+
+    ws['B2'] = str(float(input("Exit angle in deg:")))
+    ws['c2'] = "deg"
+
+    ws['B3'] = str(float(input("Throat angle in deg:")))
+    ws['c3'] = "deg"
+
+    ws['B4'] = str(str(A2)) #area exit
+    ws['c4'] = "m^2"
+
+    ws['B5'] = str(pi*(float(input("Combustion Chamber radius in INCHES:"))**2)) #area inlet
+    ws['C5'] = "in^2"
+
+    ws['B6'] = str(At)
+    ws['C6'] = "m^2" #area throat
+
+    ws['B7'] = str(float(input("Converging angle in degrees:")))
+    ws['C7'] = "deg"
+
+    ws['B8'] = str(str(float(input("Rao Throat radius in INCHES:")))+" in")
+    ws['C8'] = "in"
+
+    wb.save("partgenout.xlsx")
+elif(genfileq.lower()=="no"):
+    print("Goodbye!")
+else:
+    print("Input not recognized")
